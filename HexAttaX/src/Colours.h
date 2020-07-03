2cdae72da8eadd2cb0e128cac1ab4970199fb95e
gsproston <gsproston94@gmail.com>
2020-07-04 00:37:31 +0100

    Lighter tile hover colour

--- HexAttaX/src/Colours.h
+++ HexAttaX/src/Colours.h
@@ -8,6 +8,6 @@ namespace Colours
 	const sf::Color screenBackground(220, 220, 220);
 
 	const sf::Color tileBackground(230, 230, 230);
-	const sf::Color tileHover(200, 200, 200);
+	const sf::Color tileHover(220, 220, 220);
 	const sf::Color tileOutline(160, 160, 160);
 }
